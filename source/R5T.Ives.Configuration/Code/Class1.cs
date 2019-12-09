6af33b144ce69a0420be77f572d642ec6e38ffd6
MinexAutomation <minex.automation@gmail.com>
2019-12-08 18:31:43 -0800

    Added direct configuration-based implementation of the configuration-name provider service.

--- source/R5T.Ives.Configuration/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Ives.Configuration
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Ives.Configuration/Code/Services/Classes/DirectConfigurationBasedConfigurationNameProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+
+namespace R5T.Ives.Configuration
+{
+    /// <summary>
+    /// Provides the configuration name value draw directly from the <see cref="IConfiguration"/> instance.
+    /// This is useful during two-stage application startup DI-container configuration. The initial stage can add the default appsettings.json file, in which is contained the name of the configuration that should be run.
+    /// The configuration name specified in the default appsettings.json file can be used to add the appsettings.{configuration name}.json file to the configuration for the second-stage.
+    /// </summary>
+    public class DirectConfigurationBasedConfigurationNameProvider : IConfigurationNameProvider
+    {
+        public const string ConfigurationNameConfigurationKey = "ConfigurationName";
+
+
+        private IConfiguration Configuration { get; }
+
+
+        public DirectConfigurationBasedConfigurationNameProvider(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public string GetConfigurationName()
+        {
+            string configurationName = this.Configuration[DirectConfigurationBasedConfigurationNameProvider.ConfigurationNameConfigurationKey];
+            return configurationName;
+        }
+    }
+}
