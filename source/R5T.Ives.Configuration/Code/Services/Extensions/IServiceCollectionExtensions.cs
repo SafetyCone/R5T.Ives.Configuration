e938e45b084a7506c63bd07765c1703b239e61d9
MinexAutomation <minex.automation@gmail.com>
2020-06-08 15:04:14 -0700

    Added IServiceCollection extensions.

---
+++ source/R5T.Ives.Configuration/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Ives.Configuration
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="DirectConfigurationBasedConfigurationNameProvider"/> implementation of <see cref="IConfigurationNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDirectConfigurationBasedConfigurationNameProvider(this IServiceCollection services,
+            IServiceAction<IConfiguration> configurationAction)
+        {
+            services
+                .AddSingleton<IConfigurationNameProvider, DirectConfigurationBasedConfigurationNameProvider>()
+                .Run(configurationAction)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DirectConfigurationBasedConfigurationNameProvider"/> implementation of <see cref="IConfigurationNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IConfigurationNameProvider> AddDirectConfigurationBasedConfigurationNameProviderAction(this IServiceCollection services,
+            IServiceAction<IConfiguration> configurationAction)
+        {
+            var serviceAction = ServiceAction<IConfigurationNameProvider>.New(() => services.AddDirectConfigurationBasedConfigurationNameProvider(
+                configurationAction));
+
+            return serviceAction;
+        }
+    }
+}
